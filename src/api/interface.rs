1918c395de9f9922459ec34bff8ef21ac3c691ad
MA-Thomas <35780950+MA-Thomas@users.noreply.github.com>
2024-08-19 15:15:05 -0400

    housekeeping after HBankCommunication refactor

--- src/api/interface.rs
+++ src/api/interface.rs
@@ -1,9 +1,11 @@
+use std::path::PathBuf;
 use super::data_manager::DataManager;
 use super::archive_system::ArchiveSystem;
 use super::code_storage::CodeStorage;
 use crate::data_management::cohort_manager::CohortManager;
 use crate::data_management::synthetic_data_generator::SyntheticDataGenerator;
 use super::shared_models::*;
+use crate::api_prelude::CohortSummary;
 
 pub struct HBankInterface {
     data_manager: DataManager,
@@ -14,18 +16,18 @@ pub struct HBankInterface {
 }
 
 impl HBankInterface {
-    pub fn new() -> Self {
+    pub fn new(base_data_path: PathBuf) -> Self {
         Self {
             data_manager: DataManager::new(),
             archive_system: ArchiveSystem::new(),
             code_storage: CodeStorage::new(),
             cohort_manager: CohortManager::new(),
-            synthetic_data_generator: SyntheticDataGenerator::new(),
+            synthetic_data_generator: SyntheticDataGenerator::new(base_data_path),
         }
     }
 
     pub fn submit_code(&self, submission: CodeSubmission) -> Result<String, String> {
-        self.code_storage.store_submission(submission)
+        Ok(self.code_storage.store_submission(submission))
     }
 
     pub fn get_code_submission(&self, job_id: &str) -> Result<CodeSubmission, String> {
@@ -39,6 +41,7 @@ impl HBankInterface {
 
     pub fn setup_synthetic_data(&self, setup: SyntheticDataSetup) -> Result<SyntheticDataSetup, String> {
         self.synthetic_data_generator.setup_synthetic_data(&setup)
+            .map_err(|e| e.to_string())
     }
 
     pub fn submit_analysis_result(&self, result: AnalysisResult) -> Result<(), String> {
